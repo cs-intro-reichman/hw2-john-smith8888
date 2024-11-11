242293771c74c0754c685e9b3c03546b812f8af2
john-smith8888 <johnsmith8aug88@gmail.com>
2024-11-11 16:36:15 +0200

    First attempt to solve TimeCalc.

--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,32 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+        
+        // Shamelessly copied argument intake from previous assignment
+        int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
+		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
+        int mintoadd = Integer.parseInt(args[1]);
+        // System.out.println("Hours: " + hours + " Minutes: " + minutes + " minutes to add: " + mintoadd);
+
+        //calculation:
+        int totalminutes = hours*60 + minutes + mintoadd;
+        int nuhours = totalminutes / 60;
+        int numins = totalminutes - nuhours*60;
+        nuhours = nuhours % 24 ;
+
+        // System.out.println("New Hours: " + nuhours + " Minutes: " + numins);
+
+        // time display: basically adding zeros to hours and / or minutes under 10
+
+        String sMinutes = "" + numins;
+        if (numins < 10) {
+			sMinutes = "0" + numins;
+		}
+
+        String sHours = "" + nuhours;
+        if (nuhours < 10) {
+			sHours = "0" + nuhours;
+		}
+
+        System.out.println(sHours + ":"+sMinutes);
     }
 }
